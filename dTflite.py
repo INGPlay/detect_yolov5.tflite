7b2319db0682c491c2c9c07e87737630d1d1a04a
INGPlay <de_fer@naver.com>
2022-02-20 01:54:40 +0900

    home commit - 실시간에 맞게 프레임이 지나가도록 수정

--- dTflite.py
+++ dTflite.py
@@ -1,5 +1,3 @@
-from tkinter import Scale
-from turtle import width
 try:  # https://coral.ai/docs/edgetpu/tflite-python/#update-existing-tf-lite-code-for-the-edge-tpu
     from tflite_runtime.interpreter import Interpreter, load_delegate
 except ImportError:
@@ -23,7 +21,7 @@ def cropImageCenter(image, width, height) :
     sliY = int((originalHeight - height) * 0.5)
 
     image = image[sliX : sliX + width, sliY : sliY + height]
-
+    
     return image
 
 def draw_text(img, text,
@@ -44,114 +42,124 @@ def draw_text(img, text,
 
     return text_size
 
+def detectImage(image, interpreter, inputDetails, imageSize, thres, labels) :
+
+    image = cropImageCenter(image=image, width=imageSize, height=imageSize)
+    # plt.imshow(image); plt.show()
+
+    # quantinzation
+    scale, zeroPoint = inputDetails["quantization"]
+
+    qImage = np.uint8(image / scale + zeroPoint)
+
+    #input
+    interpreter.set_tensor(inputDetails['index'], np.uint8([qImage]))
+
+    interpreter.invoke()
+
+    # output
+    outputDetails = interpreter.get_output_details()[0]
+    output = interpreter.get_tensor(outputDetails['index'])[0]
+    scale, zeroPoint = inputDetails["quantization"]
+    output = (output.astype(np.float32) - zeroPoint) * scale
+
+    for i in range(len(output)) :
+        conf = output[i][4]
+
+        if conf >= thres :
+            w = round(output[i][2] * imageSize)
+            h = round(output[i][3] * imageSize)
+            x = round(output[i][0] * imageSize)
+            y = round(output[i][1] * imageSize)
+
+            cv2.rectangle(image, (int(x),int(y),int(w),int(h)),(0,255,0), 2)
+            cls = output[i][5:].argmax()
+            label = labels[cls]
+
+            draw_text(image, f"{label} {conf:.2f}",
+                pos= (int(x), int(y)), 
+                font= cv2.FONT_HERSHEY_DUPLEX, 
+                font_scale = 1, 
+                text_color = (0, 0, 255), 
+                font_thickness = 1
+            )
+
+    cv2.imshow("video", image)
+
 
 def main() :
 
     os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
 
     imageSize = 320
-    thres = 0.4
+    thres = 0.5
 
     #url = "rtsp://admin:worldcns\!@192.168.1.12:554/profile1/media.smp"
     url = "People - 84973.mp4"
-    model = 'yolov5n-int8-320_edgetpu.tflite'
+    model = 'yolov5n-int8-320_edgetpu.tflite'        
 
     cap = cv2.VideoCapture(url)
-
-    a = cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
-    print(a)
-    w = cap.set(cv2.CAP_PROP_FRAME_WIDTH, imageSize)
-    h = cap.set(cv2.CAP_PROP_FRAME_HEIGHT, imageSize)
-    f = cap.set(cv2.CAP_PROP_FPS, 30000)
-    print(w, h, f)
 
     videoWidth = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     videoHeight = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     fps = cap.get(cv2.CAP_PROP_FPS)
-    print(f"Width : {videoWidth}, Height : {videoHeight}, FPS : {fps}")
+    print(f"Width : {videoWidth}, Height : {videoHeight}, evaluedFps : {fps}")
 
-    interpreter = tf.lite.Interpreter(
-        model_path=model,
-        experimental_delegates=[load_delegate('edgetpu.dll')])
+    # try load_delegate('edgetpu.dll')
+    try :
+        interpreter = Interpreter(
+            model_path=model,
+            experimental_delegates=[load_delegate('edgetpu.dll')])
+    except OSError :
+        interpreter = Interpreter(
+            model_path=model)
 
     interpreter.allocate_tensors()
 
     inputDetails = interpreter.get_input_details()[0]
-
+    if not cap.read() :
+        print("Can not read")
+        sys.exit()
 
     # Label load
-    fs = open("labels.txt", "r")
     labels = []
-    lines = fs.readlines()
-    for l in lines :
-        l = l.strip()
-        labels.append(l)
+    with open("labels.txt", "r") as fs :
+        lines = fs.readlines()
+        for l in lines :
+            l = l.strip()
+            labels.append(l)
 
-    fs.close()
-
+    frameSecond = 1.0/fps
+    endTime = 0
     # Frame repeat
     while True :
         # timer start
-        start_t = timeit.default_timer()   
+        frameStartTime = timeit.default_timer()
 
         ret, image = cap.read()
         if not ret :
-            break
+            break 
+        
+        # Don't think about zero frame and fps over
+        if frameSecond > frameStartTime - endTime :
+            continue
 
-        if not cap.read() :
-            print("Can not read")
-            sys.exit()
-
-        image = cropImageCenter(image=image, width=imageSize, height=imageSize)
-        # plt.imshow(image); plt.show()
-
-        # quantinzation
-        scale, zeroPoint = inputDetails["quantization"]
-
-        qImage = np.uint8(image / scale + zeroPoint)
-
-        #input
-        interpreter.set_tensor(inputDetails['index'], np.uint8([qImage]))
-
-        interpreter.invoke()
-
-        # output
-        outputDetails = interpreter.get_output_details()[0]
-        output = interpreter.get_tensor(outputDetails['index'])[0]
-        scale, zeroPoint = inputDetails["quantization"]
-        output = (output.astype(np.float32) - zeroPoint) * scale
-
-        for i in range(len(output)) :
-            conf = output[i][4]
-            print(f"conf : {conf}")
-            if conf >= thres :
-                w = round(output[i][2] * imageSize)
-                h = round(output[i][3] * imageSize)
-                x = round(output[i][0] * imageSize)
-                y = round(output[i][1] * imageSize)
-
-                cv2.rectangle(image, (int(x),int(y),int(w),int(h)),(0,255,0), 2)
-                cls = output[i][5:].argmax()
-                label = labels[cls]
-
-                draw_text(image, f"{label} {conf:.2f}",
-                    pos= (int(x), int(y)), 
-                    font= cv2.FONT_HERSHEY_DUPLEX, 
-                    font_scale = 1, 
-                    text_color = (0, 0, 255), 
-                    font_thickness = 1
-                )
-
-        cv2.imshow("video", image)
+        detectImage(
+            image= image,
+            interpreter= interpreter,
+            inputDetails= inputDetails,
+            imageSize= imageSize,
+            thres= thres,
+            labels= labels
+        )
         
         # timer end
-        terminate_t = timeit.default_timer()
-        FPS = int(1./(terminate_t - start_t ))
-        print(f"FPS : {FPS}")
+        endTime = timeit.default_timer()
+        evaluedFps = int(1./(endTime - frameStartTime))
+        print(f"evaluedFps : {evaluedFps}")
 
         if cv2.waitKey(1) == 27 :
             break
-
 
     cap.release()
     cv2.destroyAllWindows()
